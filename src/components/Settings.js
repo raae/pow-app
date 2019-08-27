cb51121182b8a6278dd43b57505d559a74349375
Benedicte Raae <raae@bgraphic.no>
2019-08-27 14:42:08 +0200

    Fix min height on content and link to /app.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -12,9 +12,9 @@ import {
 const useStyles = makeStyles((theme) => ({
   container: {
     marginTop: theme.spacing(10),
-    marginBottom: theme.spacing(5),
+    marginBottom: theme.spacing(3),
     "& + *": {
-      marginTop: theme.spacing(5),
+      marginTop: theme.spacing(2),
     },
   },
 }))
