d0d0945a67558a11bc89e605e4b727ba275328b8
Benedicte Raae <raae@bgraphic.no>
2019-08-28 21:27:52 +0200

    Fixed app links

--- src/components/appNavFooterItems.js
+++ src/components/appNavFooterItems.js
@@ -2,21 +2,41 @@ import React from "react"
 
 import {
   OpenInNew as WebsiteIcon,
-  Ballot as ProductHuntIcon,
+  Email as SupportIcon,
+  Face as TwitteIcon,
 } from "@material-ui/icons"
 
 export default [
+  // {
+  //   label: "Support POW!",
+  //   text: "on Product Hunt",
+  //   icon: <ProductHuntIcon />,
+  //   component: "a",
+  //   target: "_blank",
+  //   rel: "noopener",
+  //   href: "https://www.producthunt.com/",
+  // },
   {
-    label: "Support POW!",
-    text: "on Product Hunt",
-    icon: <ProductHuntIcon />,
+    label: "Support",
+    text: "via email",
+    icon: <SupportIcon />,
     component: "a",
     target: "_blank",
     rel: "noopener",
-    href: "https://www.producthunt.com/",
+    href: "mailto:pow@raae.codes",
   },
   {
-    label: "Open Website",
+    label: "@raae",
+    text: "on Twitter",
+    icon: <TwitteIcon />,
+    component: "a",
+    target: "_blank",
+    rel: "noopener",
+    href: "https://twitter.com/raae",
+  },
+  {
+    label: "Back to",
+    text: "Website",
     icon: <WebsiteIcon />,
     component: "a",
     target: "_blank",
