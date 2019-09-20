0bf7ef78e1cc6e29fdfe0b903d770750d328e314
Benedicte Raae <raae@raae.codes>
2019-09-20 08:06:21 +0200

    Add manifest.json as its default and used by Dappity. (#26)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const fs = require("fs")
 const MANIFEST_FILE_PATH = "public/manifest.webmanifest"
+const MANIFEST_JSON_FILE_PATH = "public/manifest.json"
 
 exports.onPostBootstrap = () => {
   const manifest = fs.readFileSync(MANIFEST_FILE_PATH, "utf8")
@@ -8,4 +9,5 @@ exports.onPostBootstrap = () => {
     "https://www.usepow.app/icons/"
   )
   fs.writeFileSync(MANIFEST_FILE_PATH, updatedManifest)
+  fs.writeFileSync(MANIFEST_JSON_FILE_PATH, updatedManifest)
 }
