4c6c20d4ef60a0fcdcd86d527952e04fd0678a5c
Benedicte Raae <raae@bgraphic.no>
2019-09-23 13:46:13 +0200

    Footer menu

--- src/components/BrandFooter.js
+++ src/components/BrandFooter.js
@@ -1,15 +1,32 @@
 import React from "react"
-import { Link } from "@material-ui/core"
+import { Link, Grid, Container } from "@material-ui/core"
 
 const BrandFooter = () => {
   return (
-    <>
-      Made with ❤ by{" "}
-      <Link target="_blank" rel="noopener" href="https://raae.codes">
-        @raae
-      </Link>{" "}
-      and family.
-    </>
+    <Container>
+      <Grid container>
+        <Grid sm={12} md={6}>
+          Made with ❤ by{" "}
+          <Link target="_blank" rel="noopener" href="https://raae.codes">
+            @raae
+          </Link>{" "}
+          and family.
+        </Grid>
+        <Grid sm={12} md={6} style={{ textAlign: "right" }}>
+          <Link target="_blank" rel="noopener" href="http://eepurl.com/gBCgT9">
+            Newsletter
+          </Link>{" "}
+          |{" "}
+          <Link
+            target="_blank"
+            rel="noopener"
+            href="https://www.producthunt.com/posts/pow-3"
+          >
+            Product Hunt
+          </Link>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
