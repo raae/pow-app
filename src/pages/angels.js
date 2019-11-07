fb9d05c9f21a06ce943b96dc3082d10d0b7d79c8
Ola Vea <ola@olavea.com>
2019-11-07 10:26:18 +0100

    imported Button and added variant= containedfrom Material-UI #36

--- src/pages/angels.js
+++ src/pages/angels.js
@@ -1,29 +1,43 @@
 import React from "react"
 import BrandLayout from "../components/BrandLayout"
 import SEO from "../components/seo"
+import { Button } from "@material-ui/core"
 
 const IndexPage = () => {
   //   console.log(window.Stripe)
 
   const stripe = window.Stripe("pk_test_XiirziufElakjoOpyuyCrPfo")
-  debugger
-  stripe.redirectToCheckout({
-    items: [
-      {
-        sku: "sku_G7M4igcIzjEhU2",
-        quantity: 1,
-      },
-    ],
-    successUrl: "https://www.usepow.app/",
-    cancelUrl: "http://localhost:8000/",
-  })
 
+  //   event.preventDefault()
+
+  const placeOrder = (sku) => {
+    stripe.redirectToCheckout({
+      items: [
+        {
+          sku,
+          quantity: 1,
+        },
+      ],
+      successUrl: "https://www.usepow.app/",
+      cancelUrl: "http://localhost:8000/",
+    })
+  }
   return (
     <BrandLayout>
       <SEO title="Home" />
       <h1>POW!</h1>
+      <div>
+        <Button
+          variant="contained"
+          onClick={() => placeOrder("sku_G7M4igcIzjEhU2")}
+        >
+          Bla
+        </Button>
+      </div>
     </BrandLayout>
   )
 }
 
 export default IndexPage
+// onClick={(event) => this.redirectToCheckout(event)}
+// sku="sku_G7M4igcIzjEhU2" name="BRONZE" price="33"
