cbb123582054842e0f0b25c5b3e5c5c2969a253b
Benedicte Raae <raae@bgraphic.no>
2019-11-16 15:24:49 +0100

    Fix window not defined

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const STRIPE_KEY = process.env.GATSBY_STRIPE_KEY
-export const BASE_URL = window ? window.location.origin : ""
+export const BASE_URL =
+  typeof window !== `undefined` ? window.location.origin : ""
