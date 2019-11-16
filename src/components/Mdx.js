2f7b2257fa10406b6b64bef235d8783b97731fe0
Benedicte Raae <raae@bgraphic.no>
2019-11-16 14:48:04 +0100

    Use mdx for angel pages

--- src/components/Mdx.js
+++ src/components/Mdx.js
@@ -5,12 +5,13 @@ import BrandCopy from "../components/BrandCopy"
 import { Link, ButtonLink } from "../components/Link"
 import Logo from "../components/Logo"
 import SignInButton from "../components/SignInButton"
+import AngelsCheckout from "../components/AngelsCheckout"
 
 const components = {
   a: Link,
 }
 
-const shortcodes = { Logo, SignInButton, ButtonLink }
+const shortcodes = { Logo, SignInButton, ButtonLink, AngelsCheckout }
 
 const Mdx = ({ children }) => {
   return (
