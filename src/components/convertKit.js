201eb12e222b26628a7957316fd36bb91539618b
Ola Vea <ola@olavea.com>
2019-12-16 13:16:02 +0100

    new link our monthly email #60

---
+++ src/components/convertKit.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const convertKit = () => {
+  return (
+  <p>Yos</p>
+  <script async data-uid="b30f4b867b" src="https://relentless-author-3087.ck.page/b30f4b867b/index.js">The Queen</script>
+)
+}
+
+export default convertKit
+
+
