e80c42b9bb6562729d99548d434288e607959a52
Benedicte Raae <raae@bgraphic.no>
2020-01-04 22:46:20 +0100

    Updated @raae tag line.

--- src/components/BrandFooter.js
+++ src/components/BrandFooter.js
@@ -5,7 +5,8 @@ const BrandFooter = () => {
   return (
     <>
       <p>
-        Made with ❤ by <Link href="https://raae.codes">@raae</Link> and family.
+        Made with ❤ by <Link href="https://raae.codes">@raae</Link> of Lilly
+        Labs.
         <br />
         Follow along by signing up for the{" "}
         <Link href="https://lillylabs.ck.page/a5f42d2b44">newsletter</Link>.
