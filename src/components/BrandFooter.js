24c5117be679892edb0d9c6b084f34ffcf97e265
Benedicte Raae <raae@bgraphic.no>
2020-01-07 21:06:03 +0100

    Changed links in footers

--- src/components/BrandFooter.js
+++ src/components/BrandFooter.js
@@ -5,8 +5,15 @@ const BrandFooter = () => {
   return (
     <>
       <p>
-        Made with ❤ by <Link href="https://raae.codes">@raae</Link> of Lilly
-        Labs.
+        Made with ❤ <br /> by{" "}
+        <Link target="_blank" rel="noopener" href="https://twitter.com/raae">
+          @raae
+        </Link>{" "}
+        of{" "}
+        <Link target="_blank" rel="noopener" href="https://lillylabs.no">
+          Lilly Labs
+        </Link>
+        .
         <br />
         Follow along by signing up for the{" "}
         <Link href="https://lillylabs.ck.page/a5f42d2b44">newsletter</Link>.
