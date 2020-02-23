00d212e3a7a33ab8d9ef6e2f57013df5f765c262
Benedicte Raae <raae@bgraphic.no>
2020-02-23 17:21:44 +0100

    Auth provider using userbase

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,11 @@
 import { withStore } from "./src/store"
 import { withTheme } from "./src/theme"
+import { withAuthProvider } from "./src/auth"
 
 export const wrapPageElement = ({ element }) => {
   return withTheme({ element })
 }
 
 export const wrapRootElement = ({ element }) => {
-  return withStore({ element })
+  return withStore({ element: withAuthProvider({ element }) })
 }
