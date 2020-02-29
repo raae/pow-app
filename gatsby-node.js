81255e74c16f3999ceec3922f84338c830a9e786
Benedicte Raae <raae@bgraphic.no>
2020-02-29 13:28:42 +0100

    No need for special manifest without blockstack

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,3 @@
-const fs = require("fs")
-const MANIFEST_FILE_PATH = "public/manifest.webmanifest"
-const MANIFEST_JSON_FILE_PATH = "public/manifest.json"
-
-exports.onPostBootstrap = () => {
-  const manifest = fs.readFileSync(MANIFEST_FILE_PATH, "utf8")
-  const updatedManifest = manifest.replace(
-    /icons\//g,
-    "https://www.usepow.app/icons/"
-  )
-  fs.writeFileSync(MANIFEST_FILE_PATH, updatedManifest)
-  fs.writeFileSync(MANIFEST_JSON_FILE_PATH, updatedManifest)
-}
-
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
 
