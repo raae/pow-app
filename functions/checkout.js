7fee86b8eed9feeb98013e0741a1a9463732ebc7
Benedicte Raae <raae@bgraphic.no>
2020-03-01 19:02:44 +0100

    Let checkout function check if request is live or not.

--- functions/checkout.js
+++ functions/checkout.js
@@ -1,13 +1,22 @@
-const constants = require("./constants")
-const stripe = require("stripe")(constants.STRIPE_SECRET_KEY)
-const request = require("request-promise-native")
+const Stripe = require("stripe")
+const axios = require("axios")
 
 exports.handler = async (req) => {
+  const { livemode } = JSON.parse(req.body)
+  const stripe = Stripe(
+    livemode
+      ? process.env.STRIPE_SECRET_KEY
+      : process.env.STRIPE_SECRET_TEST_KEY
+  )
+  const stripeSecret = livemode
+    ? process.env.STRIPE_WEBHOOK_SECRET
+    : process.env.STRIPE_WEBHOOK_TEST_SECRET
+
   try {
     const event = stripe.webhooks.constructEvent(
       req.body,
       req.headers["stripe-signature"],
-      constants.STRIPE_WEBHOOK_SECRET
+      stripeSecret
     )
 
     if (event.type === "checkout.session.completed") {
@@ -19,24 +28,21 @@ exports.handler = async (req) => {
         metadata: { userbaseId: userbaseId },
       })
 
-      console.log(JSON.stringify(customer, null, 2))
-
-      // set payment flag on user's protected profile
-      await request({
-        method: "POST",
-        uri: "https://v1.userbase.com/v1/admin/users/" + userbaseId,
-        auth: {
-          bearer: constants.USERBASE_ADMIN_API_ACCESS_TOKEN,
-        },
-        json: true,
-        body: {
+      await axios.post(
+        "https://v1.userbase.com/v1/admin/users/" + userbaseId,
+        {
           protectedProfile: {
             stripeEmail: customer.email,
             stripePlanId: planId,
             stripeCustomerId: customer.id,
           },
         },
-      }).promise()
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.USERBASE_ADMIN_API_ACCESS_TOKEN}`,
+          },
+        }
+      )
     }
 
     console.log(`Stripe webhook succeeded`)
