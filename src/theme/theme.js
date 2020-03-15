15edcdc1f50c4e54e51bb8a354af7ac00c74d261
Benedicte Raae <raae@bgraphic.no>
2020-03-15 17:37:58 +0100

    Deafult app bare theme overrides and props

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -17,41 +17,59 @@ let theme = createMuiTheme({
       main: secondaryColor,
     },
   },
-  props: {
-    MuiLink: {
-      color: "secondary",
-    },
-    MuiTextField: {
-      InputLabelProps: { shrink: true },
-    },
-  },
-  overrides: {
-    MuiMenuItem: {
-      root: {
-        minHeight: "48px !important",
-      },
-    },
-    MuiAlert: {
-      action: {
-        marginTop: "8px",
-        marginRight: 0,
-        alignItems: "flex-start",
-      },
-    },
-    MuiLink: {
-      root: {
-        fontWeight: 500,
-        "&:hover": {
-          color: "currentColor",
-          transition: "color 0.2s",
-        },
-      },
-      underlineHover: {
-        textDecoration: "underline",
-      },
-    },
-  },
 })
+
+console.log(theme.props)
+
+theme.props = {
+  MuiLink: {
+    color: "secondary",
+  },
+  MuiButton: {
+    disableElevation: true,
+  },
+  MuiAppBar: {
+    elevation: 0,
+    color: "default",
+  },
+  MuiTextField: {
+    InputLabelProps: { shrink: true },
+  },
+}
+
+theme.overrides = {
+  MuiMenuItem: {
+    root: {
+      minHeight: "48px !important",
+    },
+  },
+  MuiAppBar: {
+    colorDefault: {
+      backgroundColor: theme.palette.background.paper,
+      borderTop: `4px solid ${theme.palette.primary.main}`,
+      borderBottom: `1px solid ${theme.palette.grey[200]}`,
+    },
+  },
+  MuiAlert: {
+    action: {
+      marginTop: "8px",
+      marginRight: 0,
+      alignItems: "flex-start",
+    },
+  },
+  MuiLink: {
+    root: {
+      fontWeight: 500,
+      "&:hover": {
+        color: "currentColor",
+        transition: "color 0.2s",
+      },
+    },
+    underlineHover: {
+      textDecoration: "underline",
+    },
+  },
+}
 
 theme = responsiveFontSizes(theme)
 
