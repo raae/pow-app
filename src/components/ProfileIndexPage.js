2a7f6b07cba189a86c5a835bf039c56976080af7
Ola Vea <ola@olavea.com>
2020-04-14 10:49:52 +0200

    moved <BrandLayout ++ to ProfileIndexPage.js

--- src/components/ProfileIndexPage.js
+++ src/components/ProfileIndexPage.js
@@ -1,4 +1,13 @@
 import React from "react"
+import { Box } from "@material-ui/core"
+import { Alert } from "@material-ui/lab"
+import BrandLayout from "../components/BrandLayout"
+import Profile from "../components/Profile"
+import PaymentForm from "../components/PaymentForm"
+import SettingsForm from "../components/SettingsForm"
+import { Link } from "../components/Link"
+import { useQueryParam } from "../utils/useQueryParam"
+//FAb below
 import ProfileRouter from "../components/ProfileRouter"
 import { Link as GatsbyLink } from "gatsby"
 import {
@@ -18,25 +27,47 @@ import EditNoteIcon from "@material-ui/icons/Edit"
 // <Card>
 //       <CardHeader
 const ProfileIndexPage = () => {
+  const paymentStatus = useQueryParam("payment")
   return (
-    <Card>
-      <div>yo</div>
-      <CardHeader
-        action={
-          <Fab
-            color="secondary"
-            size="large"
-            aria-label="Edit note"
-            component={GatsbyLink}
-            to={`/profile/edit`}
-            // className={classes.submit}
-          >
-            <EditNoteIcon />
-          </Fab>
-        }
-      />
-      <ProfileRouter />
-    </Card>
+    <BrandLayout variant="app">
+      {paymentStatus && (
+        <Alert severity="warning">
+          Check the payment section at the bottom of the page.
+        </Alert>
+      )}
+      <Box>
+        <h1>Profile</h1>
+        <Profile />
+      </Box>
+      <Box my={6}>
+        <h1>Help make POW! better</h1>
+        <Typography component="div">
+          <p>
+            It would be very helpful if I could get 30 minutes of your time.
+          </p>
+          <p>
+            <Link href="https://calendly.com/raae/pow-user">
+              Follow this link to find a time that works for you (and me).
+            </Link>
+          </p>
+          <p>
+            <small>
+              The session will be done via Zoom video conferencing, but it can
+              be done audio only if that is more comfortable for you. There is
+              also no need for you to sign up for an account with Zoom.
+            </small>
+          </p>
+        </Typography>
+      </Box>
+      <Box my={6}>
+        <h1>Settings</h1>
+        <SettingsForm />
+      </Box>
+      <Box my={6}>
+        <h1>Payment</h1>
+        <PaymentForm standalone />
+      </Box>
+    </BrandLayout>
   )
 }
 
