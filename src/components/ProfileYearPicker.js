251433dc803b6aacff9b3d6664f2f4a7c6c42155
Ola Vea <ola@olavea.com>
2020-04-14 08:16:33 +0200

    I started on the event in ProfileYearPicker

--- src/components/ProfileYearPicker.js
+++ src/components/ProfileYearPicker.js
@@ -1,1 +1,35 @@
+//import sausage meat
+import React from "react"
 
+// make your  sausage or whatever your trying to make
+class ProfileYearPicker extends React.Component {
+  iPickedAYear = React.createRef()
+  // a property set to an arrow function
+  // property goToYear is bound to the instance
+  goToStore = (event) => {
+    // 1. stop
+    event.preventDefault()
+    // 2. get the year from form
+    //    const email=
+    //    const pickedYear = this.iPickedAYear.current.value;
+    // 3. Change the year in userbase = timeShip API
+    //    this.props.history.push(`/year/${pickedYear}`);
+
+    console.log("picker")
+  }
+  render() {
+    return (
+      <form className="year-picker" onSubmit={this.goToYear}>
+        <input
+          type="text"
+          ref={this.iPickedAYear}
+          required
+          placeholder="1554"
+        />
+        <button type="submit">Go To Year</button>
+      </form>
+    )
+  }
+}
+// in order to surface it to other files in your application
+export default ProfileYearPicker
