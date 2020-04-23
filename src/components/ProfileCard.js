c4bb9e3a64191fb076f38c566a25ca417e72855e
Ola Vea <ola@olavea.com>
2020-04-23 07:57:58 +0200

    New profileEditEmailPage #116  (#125)
    * I set up a new path, but the path "...profile/edit" does not work in localhost @raae
    
    * I re-wrote ProfileEditPage to be a class
    
    * I started on the email-edit-form #116
    
    * ProfileIndexPage
    
    * Layout WesBosStyle #116
    
    * props WesBosStyle #116
    
    * video 10 reached WesBos #116
    
    * I started on the event in ProfileYearPicker
    
    * Not really  working, but
    
    * Not Really Workin, but kinda screenshot 180 #116
    
    * Fab works, button doesn't work
    
    * moved <BrandLayout ++ to ProfileIndexPage.js
    
    * and deleted in profile.js #116
    
    * The customer get's sent back 2 /profile from /edit
    
    * setState the WesBosWay #116
    
    * <GatsbyLink to="/profile
    
    * started Create place to hold the email w useState
    
    * Make Basic Form with Kent C. Dodds20 #116
    
    * Send that input to Daniel V.'s Userbase
    
    * Send that customer back to /profile #116
    
    * material-ui buttons
    
    * material-ui on input and buttons
    
    * delete lots + fix the input nested in label #116
    
    * createReset(event)
    
    * ES-lint
    
    * Rename component
    
    * Remove unused code
    
    * Refactor submit/reset to AppBar design.
    
    * Turn form into Paper component.
    
    * Refactor to functional components
    
    * Remove unused code
    
    * Add title
    
    * Add title and helper text.
    
    * Rename Profile component
    
    * Redesign edit button
    
    * Rename ProfileEditPage component
    
    Co-authored-by: Benedicte Raae <raae@bgraphic.no>

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,23 +1,31 @@
 import React from "react"
+import { Link as GatsbyLink } from "gatsby"
 import {
   Avatar,
+  IconButton,
   Card,
   CardHeader,
   CardContent,
   Typography,
+  Menu,
+  MenuItem,
   FormControlLabel,
   Switch,
   makeStyles,
 } from "@material-ui/core"
 import AccountCircle from "@material-ui/icons/AccountCircle"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
 
 import { useAuthState, useAuthActions } from "../auth"
 
 const useStyles = makeStyles((theme) => ({}))
 
-const Profile = () => {
+const ProfileCard = () => {
   const classes = useStyles()
   const { user } = useAuthState()
+
+  // Handle newsletter subscription change
+
   const { updateUser } = useAuthActions()
   const userProfile = user.profile || {}
   const userProfileProtected = user.protectedProfile || {}
@@ -30,6 +38,18 @@ const Profile = () => {
     updateUser({ email: userEmail, profile: { ...userProfile, ...profile } })
   }
 
+  // Open / close menu
+
+  const [menuAnchorEl, setMenuAnchorEl] = React.useState(null)
+
+  const handleOpenMenu = (event) => {
+    setMenuAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setMenuAnchorEl(null)
+  }
+
   if (!user) return null
 
   return (
@@ -39,6 +59,29 @@ const Profile = () => {
           <Avatar aria-label="recipe" className={classes.avatar}>
             <AccountCircle />
           </Avatar>
+        }
+        action={
+          <>
+            <IconButton
+              aria-label="settings"
+              aria-controls="simple-menu"
+              onClick={handleOpenMenu}
+            >
+              <MoreVertIcon />
+            </IconButton>
+
+            <Menu
+              id="simple-menu"
+              anchorEl={menuAnchorEl}
+              keepMounted
+              open={Boolean(menuAnchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem component={GatsbyLink} to={`/profile/edit`}>
+                Edit email address
+              </MenuItem>
+            </Menu>
+          </>
         }
         title={<strong>{user.username}</strong>}
         subheader={userEmail}
@@ -65,4 +108,4 @@ const Profile = () => {
   )
 }
 
-export default Profile
+export default ProfileCard
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,26 +1,18 @@
 import React, { useEffect } from "react"
 import { navigate } from "gatsby"
-import { Box, Typography } from "@material-ui/core"
-import { Alert } from "@material-ui/lab"
+import { Router } from "@reach/router"
 
 import { useAuthState } from "../auth"
 import { useDataState } from "../database"
 import { CycleProvider } from "../cycle"
 
-import { useQueryParam } from "../utils/useQueryParam"
-
 import SEO from "../components/Seo"
 import Loading from "../components/Loading"
 
-import BrandLayout from "../components/BrandLayout"
-import Profile from "../components/Profile"
-import PaymentForm from "../components/PaymentForm"
-import SettingsForm from "../components/SettingsForm"
-import { Link } from "../components/Link"
+import ProfileIndexPage from "../components/ProfileIndexPage"
+import ProfileEditEmailPage from "../components/ProfileEditEmailPage"
 
 const ProfilePage = () => {
-  const paymentStatus = useQueryParam("payment")
-
   const { user, isPending: authIsPending } = useAuthState()
   const { isPending: dataIsPending, entries, settings } = useDataState()
 
@@ -32,55 +24,21 @@ const ProfilePage = () => {
 
   if (!user || dataIsPending) {
     return (
-      <>
+      <div>
         <SEO title="Loading..." />
         <Loading fullScreen />
-      </>
+      </div>
     )
   }
 
   return (
     <CycleProvider entries={entries} settings={settings}>
       <SEO title="Profile" />
-      <BrandLayout variant="app">
-        {paymentStatus && (
-          <Alert severity="warning">
-            Check the payment section at the bottom of the page.
-          </Alert>
-        )}
-        <Box>
-          <h1>Profile</h1>
-          <Profile />
-        </Box>
-        <Box my={6}>
-          <h1>Help make POW! better</h1>
-          <Typography component="div">
-            <p>
-              It would be very helpful if I could get 30 minutes of your time.
-            </p>
-            <p>
-              <Link href="https://calendly.com/raae/pow-user">
-                Follow this link to find a time that works for you (and me).
-              </Link>
-            </p>
-            <p>
-              <small>
-                The session will be done via Zoom video conferencing, but it can
-                be done audio only if that is more comfortable for you. There is
-                also no need for you to sign up for an account with Zoom.
-              </small>
-            </p>
-          </Typography>
-        </Box>
-        <Box my={6}>
-          <h1>Settings</h1>
-          <SettingsForm />
-        </Box>
-        <Box my={6}>
-          <h1>Payment</h1>
-          <PaymentForm standalone />
-        </Box>
-      </BrandLayout>
+
+      <Router basepath="/profile">
+        <ProfileIndexPage path="/" />
+        <ProfileEditEmailPage path="/edit" />
+      </Router>
     </CycleProvider>
   )
 }
