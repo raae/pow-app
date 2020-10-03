a1e042364bf403d430c920f84f3648707ead3521
Benedicte Raae <raae@raae.codes>
2020-10-03 15:33:08 +0200

    Temporary password (#187)
    * Extra page with recover password functionality.

---
+++ src/pages/recover.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
+import userbase from "userbase-js"
+import { Button, TextField, FormGroup } from "@material-ui/core"
+import Alert from "@material-ui/lab/Alert"
+
+import { SEO, Loading } from "../features/app"
+import { selectAuthIsPending, selectIsAuthenticated } from "../features/auth"
+import { HOME } from "../features/navigation"
+import PageTemplate from "../templates/page"
+import { navigate } from "gatsby"
+
+const STATUS = {
+  PENDING: "Pending",
+  IDLE: "Idle",
+}
+
+const StuffPage = () => {
+  const authIsPending = useSelector(selectAuthIsPending)
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+
+  const [status, setStatus] = useState(STATUS.IDLE)
+  const disabled = status === STATUS.PENDING
+
+  const handleTemporaryPassword = async (event) => {
+    event.preventDefault()
+    try {
+      const username = event.target.elements.usernameInput.value
+      setStatus(STATUS.PENDING)
+      await userbase.forgotPassword({
+        username,
+      })
+      alert("Email sent. Check you inbox")
+      setStatus(STATUS.IDLE)
+    } catch (error) {
+      alert(error.message)
+      setStatus(STATUS.IDLE)
+    }
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate(HOME.to)
+    }
+  }, [isAuthenticated])
+
+  if (authIsPending || isAuthenticated) {
+    return <Loading fullScreen />
+  }
+
+  return (
+    <PageTemplate>
+      <SEO title="Reset" />
+
+      <h1>Reset password</h1>
+
+      <Alert severity="warning">
+        This page should only be used if directed here by POW! Support.
+      </Alert>
+
+      <p>
+        This might work if you are using the same device and browser you have
+        logged in with before, and you choose remember me the last time you
+        successfully logged in from that device and browser.
+      </p>
+
+      <form onSubmit={handleTemporaryPassword}>
+        <FormGroup>
+          <TextField
+            id="usernameInput"
+            type="text"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Username"
+            name="username"
+            autoComplete="username"
+            disabled={disabled}
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="secondary"
+            fullWidth
+            disabled={disabled}
+          >
+            Send temporary password
+          </Button>
+        </FormGroup>
+      </form>
+    </PageTemplate>
+  )
+}
+
+export default StuffPage
---
+++ src/pages/stuff.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react"
+import { useSelector } from "react-redux"
+import userbase from "userbase-js"
+import { Button, TextField, FormHelperText } from "@material-ui/core"
+import Alert from "@material-ui/lab/Alert"
+
+import { SEO, Loading } from "../features/app"
+import { selectAuthIsPending } from "../features/auth"
+import PageTemplate from "../templates/page"
+
+const StuffPage = () => {
+  const authIsPending = useSelector(selectAuthIsPending)
+
+  const [status, setStatus] = useState("INITIAL")
+  const [error, setError] = useState(null)
+  const disabled = status === "PENDING"
+  const displayError = status === "ERROR" && error
+
+  const handleTemporaryPassword = async (event) => {
+    event.preventDefault()
+    try {
+      const username = event.target.elements.usernameInput.value
+      setStatus("PENDING")
+      const user = await userbase.forgotPassword({
+        username,
+      })
+      console.log(user)
+      setStatus("EMAIL_SENT")
+    } catch (error) {
+      console.log(error)
+      setStatus("ERROR")
+      setError(error)
+    }
+  }
+
+  if (authIsPending) {
+    return <Loading />
+  }
+
+  return (
+    <PageTemplate>
+      <SEO title="Secret" />
+      <h1>Support Stuff</h1>
+      <Alert severity="warning">
+        This page should only be used if directed here by POW! support.
+      </Alert>
+      <h2>Password Recovery</h2>
+      <p>
+        This might work if you are using the same device and browser you have
+        logged in with before, and you choose remember me the last time you
+        successfully logged in.
+      </p>
+      <form onSubmit={handleTemporaryPassword}>
+        <TextField
+          id="usernameInput"
+          type="text"
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          label="Username"
+          name="username"
+          autoComplete="username"
+          disabled={disabled}
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          fullWidth
+          disabled={disabled}
+        >
+          Send temporary password
+        </Button>
+        {displayError && <FormHelperText>{error.message}</FormHelperText>}
+      </form>
+    </PageTemplate>
+  )
+}
+
+export default StuffPage
