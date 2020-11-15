08e6f8f3a3208eabbf2802c88b37fb33533585b8
Benedicte Raae <raae@bgraphic.no>
2020-11-15 09:18:01 +0100

    Remove unsued export

--- src/features/settings/index.js
+++ src/features/settings/index.js
@@ -1,5 +1,4 @@
 export { default as SettingsCard } from "./SettingsCard"
-export { default as SettingsForm } from "./SettingsMensesTagForm"
 export { default as SettingsMensesTagForm } from "./SettingsMensesTagForm"
 
 export * from "./slice"
