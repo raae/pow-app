5a97c7148fbe28ef4713f0b0489efe3a0ac8b86c
Ola Vea <ola@olavea.com>
2020-12-30 09:12:22 +0100

    now name is right in 🦄 switch statement

--- src/features/onboarding/Welcome.js
+++ src/features/onboarding/Welcome.js
@@ -70,7 +70,7 @@ const Welcome = () => {
         </Alert>
       )}
       {welcomeCompleted && !addToHomeScreenCompleted && (
-        <Alert onClose={handleClose("newsletterOff")}>
+        <Alert onClose={handleClose("addToHomeScreen")}>
           <AlertTitle severity="info">Add POW! To Your Home Screen </AlertTitle>
           <Typography component="div">
             <p>
