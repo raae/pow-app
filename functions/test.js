3a5da4d6f1544a30296ab56dc7b982529ba71e5a
Benedicte Raae <raae@bgraphic.no>
2021-01-12 08:02:47 +0100

    Restructure

--- functions/test.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-  const { payload } = JSON.parse(event.body)
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello TEST" }),
-  }
-}
---
+++ functions/users/utils/userbase.js
@@ -0,0 +1,49 @@
+const axios = require("axios")
+
+const { USERBASE_ADMIN_API_ACCESS_TOKEN } = process.env
+
+const getUserbaseUser = async ({ userbaseId }) => {
+  const { data } = await axios.get(
+    "https://v1.userbase.com/v1/admin/users/" + userbaseId,
+    {
+      headers: {
+        Authorization: `Bearer ${USERBASE_ADMIN_API_ACCESS_TOKEN}`,
+      },
+    }
+  )
+  return data
+}
+
+const updateUserbaseProtectedProfile = async ({
+  userbaseId,
+  protectedProfile,
+}) => {
+  // Fetch this again to make sure we have the latest
+  const user = await getUserbaseUser({ userbaseId })
+
+  // Updating the user has no response
+  await axios.post(
+    "https://v1.userbase.com/v1/admin/users/" + userbaseId,
+    {
+      protectedProfile: {
+        ...user.protectedProfile,
+        ...protectedProfile,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${USERBASE_ADMIN_API_ACCESS_TOKEN}`,
+      },
+    }
+  )
+
+  console.log("Userbase Protected Profile updated", {
+    userbaseId,
+    protectedProfile,
+  })
+
+  return { userbaseId }
+}
+
+exports.getUserbaseUser = getUserbaseUser
+exports.updateUserbaseProtectedProfile = updateUserbaseProtectedProfile
