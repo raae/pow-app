22069827cfd69de622de6ca152ea319dac513305
Benedicte Raae <raae@bgraphic.no>
2021-01-30 07:37:19 +0100

    Refactor and make custom http handler.

--- functions/users/index.js
+++ functions/users/index.js
@@ -1,63 +1,15 @@
 const middy = require("@middy/core")
 const jsonBodyParser = require("@middy/http-json-body-parser")
-const httpErrorHandler = require("@middy/http-error-handler")
 const Joi = require("joi")
-const format = require("date-fns/format")
-const createError = require("http-errors")
+
 const validateHttpMethod = require("./middleware/validateHttpMethod")
 const validateJoiBodySchema = require("./middleware/validateJoiBodySchema")
 const addParamsToBody = require("./middleware/addParamsToBody")
 const secretsManager = require("./middleware/secretsManager")
+const verifyUserbaseAuthToken = require("./middleware/verifyUserbaseAuthToken")
+const httpErrorHandler = require("./middleware/httpErrorHandler")
 
-const Userbase = require("./utils/userbase")
-const ConvertKit = require("./utils/convertkit")
-
-const verifyUserbaseAuthToken = () => {
-  return {
-    before: async ({ event }) => {
-      const { body, context } = event
-      const userbase = Userbase(context.USERBASE_ADMIN_API_ACCESS_TOKEN)
-      try {
-        await userbase.verifyUserbaseAuthToken(body)
-      } catch (error) {
-        const { message } = error.response?.data || error.request?.data || error
-        throw new createError.Unauthorized("Userbase: " + message)
-      }
-    },
-  }
-}
-
-const handleUserCreated = async (body, context) => {
-  const userbase = Userbase(context.USERBASE_ADMIN_API_ACCESS_TOKEN)
-  const convertKit = ConvertKit(context.CONVERTKIT_API_SECRET)
-
-  try {
-    const userbaseUser = await userbase.getUserbaseUser(body)
-    const convertKitSubscriber = await convertKit.upsertConvertKitSubscriber({
-      formId: context.CONVERTKIT_FORM_ID,
-      email: userbaseUser.email,
-      fields: {
-        userbase_id: userbaseUser.userId,
-        user_created: format(new Date(userbaseUser.creationDate), "yyyy-MM-dd"),
-      },
-    })
-    const userbaseUserId = userbaseUser.userId
-    const convertKitSubscriberId = `${convertKitSubscriber.id}`
-
-    await userbase.updateUserbaseProtectedProfile({
-      userbaseUserId,
-      protectedProfile: { convertKitId: `${convertKitSubscriberId}` },
-    })
-
-    return {
-      userbaseUserId,
-      convertKitSubscriberId,
-    }
-  } catch (error) {
-    const { message } = error.response?.data || error.request?.data || error
-    throw new createError.InternalServerError(message)
-  }
-}
+const handleUserCreated = require("./handlers/userCreated")
 
 const userCreatedHandler = async ({ body, context }) => {
   const result = await handleUserCreated(body, context)
@@ -69,7 +21,7 @@ const userCreatedHandler = async ({ body, context }) => {
 
 const handler = middy(userCreatedHandler)
   .use(validateHttpMethod("POST"))
-  .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
+  .use(jsonBodyParser())
   .use(addParamsToBody())
   .use(
     validateJoiBodySchema({
@@ -91,6 +43,6 @@ const handler = middy(userCreatedHandler)
     })
   )
   .use(verifyUserbaseAuthToken())
-  .use(httpErrorHandler()) // handles common http errors and returns proper responses
+  .use(httpErrorHandler())
 
 module.exports = { handler }
---
+++ functions/users/middleware/httpErrorHandler.js
@@ -0,0 +1,29 @@
+const createError = require("http-errors")
+
+module.exports = () => {
+  return {
+    onError: (handler) => {
+      const error = handler.error
+
+      if (!createError.isHttpError(error)) {
+        error = createError.InternalServerError(error.message)
+      }
+
+      const { statusCode, expose, message } = error
+
+      console.warn(error.message)
+
+      const body = {
+        error: {
+          statusCode,
+          ...(expose && { message }),
+        },
+      }
+
+      handler.response = {
+        statusCode,
+        body: JSON.stringify(body),
+      }
+    },
+  }
+}
