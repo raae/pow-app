7bfe7883add9f2a596c41322a34ab16174878603
Benedicte Raae <raae@raae.codes>
2021-02-06 15:45:50 +0100

    Add quick tagging

--- src/features/timeline/TimelineItem/Tags.js
+++ src/features/timeline/TimelineItem/Tags.js
@@ -1,10 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 import classNames from "classnames"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { Chip, makeStyles } from "@material-ui/core"
+import { AddCircle as AddIcon } from "@material-ui/icons"
 
 import { selectTagsForDate } from "../../cycle"
+import { selectEntryNote, upsertEntry } from "../../entries"
 
 const useStyles = makeStyles((theme) => ({
   tag: {
@@ -22,7 +24,16 @@ const useStyles = makeStyles((theme) => ({
 const Tags = ({ date, className }) => {
   const classes = useStyles()
 
+  const dispatch = useDispatch()
+  const entryNote = useSelector((state) => selectEntryNote(state, { date }))
   const tags = useSelector((state) => selectTagsForDate(state, { date }))
+
+  const handleAddTag = (tag) => (event) => {
+    event.preventDefault()
+    // if note ends with a tag keep adding, if not add a line break.
+    const note = `${entryNote} #${tag}`
+    dispatch(upsertEntry(date, { note }))
+  }
 
   if (tags.length === 0) return null
 
@@ -31,6 +42,12 @@ const Tags = ({ date, className }) => {
       {tags
         .sort(({ count: countA }, { count: countB }) => countB - countA)
         .map(({ tag, logged, predicted }) => {
+          const addProps = {
+            // The api for chips uses delete
+            // for the icon added to the right...
+            onDelete: handleAddTag(tag),
+            deleteIcon: <AddIcon />,
+          }
           return (
             <Chip
               className={classNames(classes.tag, {
@@ -41,7 +58,7 @@ const Tags = ({ date, className }) => {
               size="small"
               key={tag}
               label={`#${tag}`}
-              component="span"
+              {...(!logged && addProps)}
             />
           )
         })}
