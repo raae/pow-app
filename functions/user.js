29afd45d5e1046451f74fb04dbd252b73e1ad5ee
Benedicte Raae <raae@raae.codes>
2021-02-12 21:30:12 +0100

    Refactor

--- functions/user.js
+++ functions/user.js
@@ -1,4 +1,4 @@
 const serverless = require("serverless-http")
-const app = require("./user/index.js")
+const app = require("./user/index.js")("/.netlify/functions/user")
 
 module.exports.handler = serverless(app)
