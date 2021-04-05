a71d9fd98aee77a5dd2852609fd72fd5bb6994ec
Benedicte Raae <raae@raae.codes>
2021-04-05 12:49:24 +0200

    Update subscription #247 (#248)
    * Rewording
    
    * Display sub info for all updatable subscriptions
    
    * Fix old plan labels

--- src/features/payment/index.js
+++ src/features/payment/index.js
@@ -1,1 +1,17 @@
-export { default as PaymentForm } from "./PaymentForm"
+import React from "react"
+import { useSelector } from "react-redux"
+
+import { selectHasOldActiveSubscription } from "../auth"
+
+import NewPaymentForm from "./PaymentForm"
+import OldPaymentForm from "./OldPaymentForm"
+
+export const PaymentForm = (props) => {
+  const hasOldActiveSubscription = useSelector(selectHasOldActiveSubscription)
+
+  if (hasOldActiveSubscription) {
+    return <OldPaymentForm {...props} />
+  } else {
+    return <NewPaymentForm {...props} />
+  }
+}
