d847c05138e9938022cf98ebfcc39d64d05ae9ad
Benedicte Raae <raae@raae.codes>
2021-05-05 11:16:08 +0200

    Disabled nav items while authenticating

--- src/features/navigation/NavMenu.js
+++ src/features/navigation/NavMenu.js
@@ -2,11 +2,17 @@ import React from "react"
 import PropTypes from "prop-types"
 import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core"
 
-const NavMenu = ({ items, onClick }) => {
+const NavMenu = ({ items, disabled, onClick }) => {
   return (
     <List>
       {items.map(({ icon, ...props }, index) => (
-        <ListItem key={index} button {...props} onClick={onClick}>
+        <ListItem
+          key={index}
+          {...props}
+          onClick={onClick}
+          disabled={disabled}
+          button
+        >
           {icon && <ListItemIcon>{icon}</ListItemIcon>}
           <ListItemText {...props} />
         </ListItem>
