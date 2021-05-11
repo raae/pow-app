7c276550cb22bde526240e57de08876da0cac079
Ola <ola@olavea.com>
2021-05-11 09:44:41 +0200

    fix: 404 page #51 (#268 )

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {Link} from "gatsby"
+
+const KrakenPage = () => {
+    return (
+      <>
+        <h1>You better get back aboard POW! because ... the Kraken.</h1>
+        <Link to="/timeline">Go to timeline</Link>
+      </>
+    )
+}
+
+export default KrakenPage;
