008e3c1a3342ca5f5d8b308d8e7c186f34c31fab
Ola <ola@olavea.com>
2021-05-12 11:27:34 +0200

    Will keep working tomorrow #51 (#269)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from "react"
 import {Link} from "gatsby"
+import { AppAuthLayout } from "../features/app"
+//import HOME from "../features/navigation/navItems"
+import HOME from "../features/navigation"
+//https://www.lego.com/en-no/product/batman-classic-tv-series-batmobile-76188
 
 const KrakenPage = () => {
     return (
       <>
-        <h1>You better get back aboard POW! because ... the Kraken.</h1>
-        <Link to="/timeline">Go to timeline</Link>
-      </>
-    )
-}
+        <AppAuthLayout title="Not Found">
 
+            <p>Jump in my old school batmobile and I'll give you a ride to my beauuuuutiful mansion for some good old fashioned fun.</p>
+
+            <Link to={HOME}>Go POW!</Link>
+        </AppAuthLayout>
+      </>
+    )}
 export default KrakenPage;
