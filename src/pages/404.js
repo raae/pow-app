115ce8d7436bcd6ecfdf09650c6140d5803b8b04
Ola <ola@olavea.com>
2021-05-13 09:51:03 +0200

    named import & <a href={HOME.to}> #51

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,7 @@
+//import HOME from "../features/navigation/navItems"
+//navigate(HOME.to)
 
+//https://www.lego.com/en-no/product/batman-classic-tv-series-batmobile-76188
 
 
 
@@ -13,21 +16,18 @@
 
 
 import React from "react"
-import {Link} from "gatsby"
 import { AppAuthLayout } from "../features/app"
-//import HOME from "../features/navigation/navItems"
-import HOME from "../features/navigation"
-//https://www.lego.com/en-no/product/batman-classic-tv-series-batmobile-76188
+import {HOME} from "../features/navigation"
 
-const KrakenPage = () => {
+const NotFoundPage = () => {
     return (
       <>
         <AppAuthLayout title="Not Found">
 
-            <p>Jump in my old school batmobile and I'll give you a ride to my beauuuuutiful mansion for some good old fashioned fun.</p>
+            <p>Jump in my old school batmobile and I'll give you a ride to my beauuuuutiful mansion for some good old fashioned fun. Or....</p>
 
-            <Link to={HOME}>Go POW!</Link>
+            <a href={HOME.to}>Go to POW!</a>
         </AppAuthLayout>
       </>
     )}
-export default KrakenPage;
+export default NotFoundPage;
