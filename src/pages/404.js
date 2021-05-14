c4782b2b92acf9ee549723e1bde752cae7baaf88
Ola <ola@olavea.com>
2021-05-14 10:17:15 +0200

    It works locally, but ... #51 (#269)

--- src/pages/404.js
+++ src/pages/404.js
@@ -18,15 +18,19 @@
 import React from "react"
 import { AppAuthLayout } from "../features/app"
 import {HOME} from "../features/navigation"
+import {StaticImage} from "gatsby-plugin-image"
 
 const NotFoundPage = () => {
     return (
       <>
         <AppAuthLayout title="Not Found">
 
-            <p>Jump in my old school batmobile and I'll give you a ride to my beauuuuutiful mansion for some good old fashioned fun. Or....</p>
+            <p>Nothing to see here, go back to <a href={HOME.to}>POW!</a></p>
+            <StaticImage
+              src="https://images.unsplash.com/photo-1554579653-17cdf3bfbca6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzkxfHxsZWdvJTIwYmF0bWFufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+            />
+            <p>Photo by <a href="https://unsplash.com/@teo">Teo Zac</a> on <a href="https://unsplash.com/@teo">Unsplash</a>.</p>
 
-            <a href={HOME.to}>Go to POW!</a>
         </AppAuthLayout>
       </>
     )}
