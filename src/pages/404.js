531e4829365a3c591c0354c3994e1a3cca703217
Ola <ola@olavea.com>
2021-05-19 11:21:46 +0200

    Fix: move the 404 image into our src/images #283

--- src/pages/404.js
+++ src/pages/404.js
@@ -19,6 +19,7 @@ import React from "react"
 import { AppAuthLayout } from "../features/app"
 import {HOME} from "../features/navigation"
 import {StaticImage} from "gatsby-plugin-image"
+//import Photo_by_Teo_Zac_unsplash_com_at_teo_2 from "../images/Photo_by_Teo_Zac_unsplash_com_at_teo_2.jpeg"
 
 const NotFoundPage = () => {
     return (
@@ -27,7 +28,7 @@ const NotFoundPage = () => {
 
             <p>Nothing to see here, go back to <a href={HOME.to}>POW!</a></p>
             <StaticImage
-              src="https://images.unsplash.com/photo-1554579653-17cdf3bfbca6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzkxfHxsZWdvJTIwYmF0bWFufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+              src="../images/Photo_by_Teo_Zac_unsplash_com_at_teo_2.jpeg"
             />
             <p>Photo by <a href="https://unsplash.com/@teo">Teo Zac</a> on <a href="https://unsplash.com/@teo">Unsplash</a>.</p>
 
