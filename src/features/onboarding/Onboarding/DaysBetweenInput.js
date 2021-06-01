35ce770839381cf3dc516383b549ca7fd783ca5c
Benedicte Raae <raae@raae.codes>
2021-06-01 15:34:58 +0200

    Add DaysBetween to incomplete form + a little recatoring

--- src/features/onboarding/Onboarding/DaysBetweenInput.js
+++ src/features/onboarding/Onboarding/DaysBetweenInput.js
@@ -1,48 +1,42 @@
-import React, { useState } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { TextField, InputAdornment } from "@material-ui/core"
-
 import { CYCLE_LENGTH_MIN_MAX, DEFAULT_CYCLE_LENGTH } from "../../settings"
 
 const DaysBetweenInput = ({
   onChange,
-  values,
+  value,
   textFieldProps,
-  defaultLength,
   minMax,
+  defaultLength,
 }) => {
-  const [value, setValue] = useState(values.daysBetween)
-
   const onChangeDaysBetween = (e) => {
-    const currentValue = parseInt(e.target.value, 10)
-    if (currentValue === -1) {
-      setValue(defaultLength - 1)
-    } else if (currentValue === 1) {
-      setValue(defaultLength + 1)
-    } else {
-      setValue(currentValue)
+    let newValue = e.target.value && parseInt(e.target.value, 10)
+
+    if (newValue === -1) {
+      newValue = defaultLength - 1
+    } else if (newValue === 1) {
+      newValue = defaultLength + 1
     }
 
-    onChange("daysBetween")(value)
+    onChange(newValue)
   }
 
   return (
-    <div>
-      <TextField
-        {...textFieldProps}
-        label="Days between menstruations"
-        value={value}
-        type="number"
-        onChange={onChangeDaysBetween}
-        placeholder={defaultLength}
-        min={minMax.min}
-        max={minMax.max}
-        fullWidth
-        InputProps={{
-          startAdornment: <InputAdornment position="start">#</InputAdornment>,
-        }}
-      />
-    </div>
+    <TextField
+      {...textFieldProps}
+      label="Days between menstruations"
+      value={value}
+      type="number"
+      onChange={onChangeDaysBetween}
+      placeholder={`${defaultLength}`}
+      min={minMax.min}
+      max={minMax.max}
+      fullWidth
+      InputProps={{
+        startAdornment: <InputAdornment position="start">#</InputAdornment>,
+      }}
+    />
   )
 }
 
