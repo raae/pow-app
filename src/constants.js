be6a91653436cd72c9e2fc1ee5ea9d08f989206f
Ola <ola@olavea.com>
2021-06-01 08:01:39 +0200

    suggestion: Move down to FATHOM_SUPPORT #302

--- src/constants.js
+++ src/constants.js
@@ -13,7 +13,6 @@ export const STRIPE_YEARLY_PLAN_ID = process.env.GATSBY_STRIPE_YEARLY_PLAN_ID
 export const SESSION_LENGTH =
   parseInt(process.env.GATSBY_USERBASE_SESSION_LENGTH) || 24 * 7 // a week
 
-export const FATHOM_EXPORT = process.env.GATSBY_FATHOM_EXPORT
 export const FATHOM_ONBOARDING_1 = process.env.GATSBY_FATHOM_ONBOARDING_1
 export const FATHOM_ONBOARDING_2 = process.env.GATSBY_FATHOM_ONBOARDING_2
 export const FATHOM_ONBOARDING_3 = process.env.GATSBY_FATHOM_ONBOARDING_3
@@ -22,6 +21,7 @@ export const FATHOM_FEEDBACK = process.env.GATSBY_FATHOM_FEEDBACK
 export const FATHOM_CHANGELOG = process.env.GATSBY_FATHOM_CHANGELOG
 export const FATHOM_MADE_BY = process.env.GATSBY_FATHOM_MADE_BY
 export const FATHOM_SUPPORT = process.env.GATSBY_FATHOM_SUPPORT
+export const FATHOM_EXPORT = process.env.GATSBY_FATHOM_EXPORT
 
 export const ENTRIES_DATABASE = { databaseName: "entries", entity: "Entry" }
 export const SETTINGS_DATABASE = { databaseName: "settings", entity: "Setting" }
